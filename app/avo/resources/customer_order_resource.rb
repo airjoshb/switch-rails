8975cdb31c023919f442cc27a872bd2418e0d9db
airjoshb <josh@gotostepone.com>
2023-09-01 17:55:02 -0700

    update index

--- app/avo/resources/customer_order_resource.rb
+++ app/avo/resources/customer_order_resource.rb
@@ -11,19 +11,20 @@ class CustomerOrderResource < Avo::BaseResource
   field :id, as: :id
   # Fields generated from the model
   field :guid, as: :text
+  field :created_at, as: :date_time
   field :order_status, as: :select, enum: CustomerOrder.order_statuses
   field :subscription_status, as: :text
   field :stripe_id, as: :text, hide_on: [:index]
   field :subscription_id, as: :text, hide_on: [:index]
   field :amount, as: :number
   field :fulfillment_method, as: :text
-  field :payment_method, as: :has_one
-  field :address, as: :has_one
+  field :payment_method, as: :has_one, hide_on: [:index]
+  field :address, as: :has_one, hide_on: [:index]
   field :customer, as: :belongs_to, sortable: true
   field :orderables, as: :has_many
   field :invoices, as: :has_many
   field :description, as: :textarea
-  field :completed_at, as: :date_time
+  field :completed_at, as: :date_time, hide_on: [:index]
   field :receipt_sent, as: :boolean
   field :receipt_sent_date, as: :date_time
   # add fields here
