22ca92c18200bc3f91c6e196b3a9d4ac7cd5dd2c
airjoshb <josh@gotostepone.com>
2024-01-25 21:00:49 -0800

    fix

--- app/controllers/create_checkout_sessions_controller.rb
+++ app/controllers/create_checkout_sessions_controller.rb
@@ -225,19 +225,22 @@ class CreateCheckoutSessionsController < ApplicationController
   end
 
   def create_invoice(invoice)
-    if invoice.subscription.present?
-      customer_order = CustomerOrder.find_by_subscription_id(invoice.subscription)
+    stripe_invoice = Stripe::Invoice.retrieve(invoice.id)
+    if stripe_invoice.subscription.present?
+      customer_order = CustomerOrder.find_by_subscription_id(stripe_invoice.subscription)
     else
-      stripe_customer = Stripe::Customer.retrieve(invoice.customer)
+      stripe_customer = Stripe::Customer.retrieve(stripe_invoice.customer)
       customer = Customer.create_with(phone: stripe_customer.phone, name: stripe_customer.name).find_or_create_by(stripe_id: stripe_customer.id)
       customer_order = customer.customer_orders.create
     end
-    new_invoice = Invoice.find_or_create_by(invoice_id: invoice.id, customer_order: customer_order)
-    time_start = Time.at(invoice.period_start.to_i)
-    time_end = Time.at(invoice.period_end.to_i)
-    new_invoice.update(subscription_id: invoice.subscription, period_start: time_start, period_end: time_end,
-      amount_due: invoice.amount_due, invoice_status: invoice.status
+    new_invoice = Invoice.find_or_create_by(invoice_id: stripe_invoice.id, customer_order: customer_order)
+    time_start = Time.at(stripe_invoice.period_start.to_i)
+    time_end = Time.at(stripe_invoice.period_end.to_i)
+    new_invoice.update(subscription_id: stripe_invoice.subscription, period_start: time_start, period_end: time_end,
+      amount_due: stripe_invoice.amount_due, invoice_status: stripe_invoice.status
     )
+    order = CustomerOrder.find(customer_order.id)
+    order.invoices << new_invoice
     puts "Created invoice ##{new_invoice.id}"
   end
 
