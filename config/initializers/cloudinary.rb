0fe919a5c739d849663c5c0cb0b53c039b7d34b0
airjoshb <josh@gotostepone.com>
2024-09-30 09:33:30 -0700

    cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,10 @@
+# Replace the 'config_from_url' string value below with your
+# product environment's credentials, available from your Cloudinary console.
+# =====================================================
+
+require 'cloudinary'
+
+Cloudinary.config_from_url(ENV.fetch('CLOUDINARY_URL'))
+Cloudinary.config do |config|
+  config.secure = true
+end
