f4b5db354251213c680106293ec343f1cbfaa11a
airjoshb <josh@gotostepone.com>
2024-09-30 12:52:47 -0700

    update

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,9 @@
 
 require 'cloudinary'
 
-Cloudinary.config_from_url("cloudinary://ENV.fetch('CLOUDINARY_KEY_ID'):ENV.fetch('CLOUDINARY_ACCESS_KEY')@airjoshb")
+Cloudinary.config_from_url("cloudinary://ENV.fetch('CLOUDINARY_URL')")
 Cloudinary.config do |config|
   config.secure = true
+  config.enhance_image_tag = true
+  config.static_file_support= false
 end
