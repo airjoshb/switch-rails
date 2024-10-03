179f4d985198ea8d31db2d9aede661c8c24d52d3
airjoshb <josh@gotostepone.com>
2024-10-03 15:04:06 -0700

    cloudinary fix

--- config/initializers/cloudinary.rb
+++
@@ -1,9 +0,0 @@
-
-require 'cloudinary'
-
-Cloudinary.config_from_url("cloudinary://ENV.fetch('CLOUDINARY_URL')")
-Cloudinary.config do |config|
-  config.secure = true
-  config.enhance_image_tag = true
-  config.static_file_support= false
-end
