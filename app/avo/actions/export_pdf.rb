eb5c6b76f76616f282958fa82d62ae63c1542d73
airjoshb <josh@gotostepone.com>
2025-10-14 16:55:56 -0700

    update

--- app/avo/actions/export_pdf.rb
+++ app/avo/actions/export_pdf.rb
@@ -10,16 +10,15 @@ class ExportPdf < Avo::BaseAction
 
     models.each do |order|
       pdf.text order.created_at.strftime("%B %d, %Y %l:%M %p")
-      pdf.text "Customer Name: #{order.customer&.name || 'N/A'}", style: :bold
+      pdf.text "#{order.customer&.name || 'N/A'}", style: :bold
+      pdf.text "#{order.address&.full_address || 'N/A'}"
       pdf.text "Fulfillment Method: #{order.fulfillment_method || 'N/A'}"
-      pdf.text "Address: #{order.address&.full_address || 'N/A'}"
       pdf.move_down 5
 
-      pdf.text "Orderables:", style: :bold
       if order.orderables.any?
-        data = [["Variation Name"]]
-        data += order.orderables.map { |o| [o.variation&.name || 'N/A'] }
-        pdf.table(data, header: true, width: 300)
+        data = [["Quantity", ""]]
+        data += order.orderables.map { |o| [o.variation&.unit_quantity || 'N/A', o.variation&.name || 'N/A'] }
+        pdf.table(data, header: false, width: 300)
       else
         pdf.text "No orderables."
       end
