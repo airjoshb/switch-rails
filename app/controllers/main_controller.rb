b91754b934eecadc813e8176ce68f0491bdaa896
airjoshb <josh@gotostepone.com>
2026-04-13 20:57:15 -0700

    update

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,14 @@ class MainController < ApplicationController
     @page = Page.find_by_slug("home")
     @categories = Category.active.categories.order(row_order: :asc)
     @all = Category.find_by_name("All")
-    @post = Post.first
-    @posts = Post.first(5).without(@post)
+
+    nav_page_slugs = Page.where(nav: true).where.not(slug: [nil, ""]).pluck(:slug)
+    posts_scope = Post.left_joins(:category)
+    if nav_page_slugs.any?
+      posts_scope = posts_scope.where("categories.slug IS NULL OR categories.slug NOT IN (?)", nav_page_slugs)
+    end
+
+    @post = posts_scope.first
+    @posts = posts_scope.where.not(id: @post&.id).limit(5)
   end
 end
